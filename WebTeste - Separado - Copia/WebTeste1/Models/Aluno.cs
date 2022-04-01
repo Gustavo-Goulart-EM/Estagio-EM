c40d9395471778c8ede9fd0d9c9272d17db49ae2
Gustavo-Goulart-EM <gustavo.goulart@escolarmanager.com.br>
2022-04-01 11:16:02 -0300

    Alterado como a máscara de CPF funciona, agora a máscara é adicionada quando o limite de 11 dígitos é atingido.

--- WebTeste - Separado - Copia/WebTeste1/Models/Aluno.cs
+++ WebTeste - Separado - Copia/WebTeste1/Models/Aluno.cs
@@ -6,7 +6,7 @@ namespace WebTeste
     public class Aluno : IEntidade
     {
         [Required(ErrorMessage = "Insira a matrícula do aluno.", AllowEmptyStrings = false)]
-        [Range(1, 999999999, ErrorMessage = "Matrícula deve estar entre 1 e 999999999")]
+        [Range(1, 999999999, ErrorMessage = "Matrícula deve estar entre 1 e 999999999.")]
         public int? Matricula { get; set; }
 
         [Required(ErrorMessage = "Insira nome do aluno.", AllowEmptyStrings = false)]
@@ -17,9 +17,9 @@ namespace WebTeste
         public EnumeradorSexo? Sexo { get; set; }
 
         [Required(ErrorMessage = "Insira a data de nascimento do aluno.", AllowEmptyStrings = false)]
-        [DataType(DataType.Date, ErrorMessage = "Isto não é uma data")]
+        [DataType(DataType.Date, ErrorMessage = "Isto não é uma data.")]
         [Display(Name = "Data de nascimento")]
-        [Range(typeof(DateTime), "01/01/1950", "01/01/2022", ErrorMessage = "O ano de nascimento do aluno deve ser pelo menos 1950 e no máximo 01/01/2022")]
+        [Range(typeof(DateTime), "01/01/1950", "01/01/2022", ErrorMessage = "O ano de nascimento do aluno deve ser pelo menos 1950 e no máximo 01/01/2022.")]
         public DateTime? Nascimento { get; set; }
 
         [StringLength(14, MinimumLength = 11, ErrorMessage = "CPF deve ter 11 dígitos.")]
